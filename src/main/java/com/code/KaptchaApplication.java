0c641554de77770b006b054c329aec80cd9ac81d
Puhuaizi <654195797@qq.com>
2018-10-16 13:16:47 +0800

    Initial commit

---
+++ src/main/java/com/code/KaptchaApplication.java
@@ -0,0 +1,17 @@
+package com.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * @author Admin
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class KaptchaApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(KaptchaApplication.class, args);
+    }
+}
